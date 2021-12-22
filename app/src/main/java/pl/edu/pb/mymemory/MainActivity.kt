37a23a08dd66c09ee664d6d862c2256d129f7da9
Patryk Walendziuk <walenda1997@gmail.com>
2021-12-22 22:54:29 +0100

    flip card when click

--- app/src/main/java/pl/edu/pb/mymemory/MainActivity.kt
+++ app/src/main/java/pl/edu/pb/mymemory/MainActivity.kt
@@ -15,10 +15,14 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "MainActivity"
     }
 
+
+
     //will be set in onCreate method
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter: MemoryBoardAdapter
 
     //choosing game mode
     private var boardSize: BoardSize = BoardSize.HARD
@@ -32,18 +36,19 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        //making object MemoryGame
-        val memoryGame = MemoryGame(boardSize)
+        //making property MemoryGame
+        memoryGame = MemoryGame(boardSize)
 
 
         //Adapter provide a binding for the data set to the views of the RecyclerView | adapt each piece of data into a view
                                             //MainActivity is context, how many elements is in our grid, list of pictures
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
             override fun onCardClicked(position: Int) {
                 Log.i(TAG, "Card clicked $position")
+                updateGameWithFlip(position)
             }
-
         } )
+        rvBoard.adapter = adapter
 
         //optimisation - size of adapter always be defined as soon as the app boots up
         rvBoard.setHasFixedSize(true)
@@ -53,5 +58,12 @@ class MainActivity : AppCompatActivity() {
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
 
 
+    }
+    //updating the memory game with attempted flip at this position
+    private fun updateGameWithFlip(position: Int) {
+        memoryGame.flipCard(position)
+
+        //notifing MemoryBoardAdapt about data change
+        adapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/pl/edu/pb/mymemory/models/MemoryGame.kt
+++ app/src/main/java/pl/edu/pb/mymemory/models/MemoryGame.kt
@@ -3,6 +3,7 @@ package pl.edu.pb.mymemory.models
 import pl.edu.pb.mymemory.utils.DEFAULT_ICONS
 
 class MemoryGame(private val  boardSize: BoardSize){
+
     val cards: List<MemoryCard>
     val numPairsFound = 0
 
@@ -11,6 +12,12 @@ class MemoryGame(private val  boardSize: BoardSize){
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
         val randomizedImages = (chosenImages + chosenImages).shuffled()
         cards = randomizedImages.map{MemoryCard(it)}
+    }
+
+    //logic
+    fun flipCard(position: Int) {
+        val card = cards[position]
+        card.isFaceUp = !card.isFaceUp
     }
 }
 
